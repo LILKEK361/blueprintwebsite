c90846c73b4da59566a6a9e6bddd0e8b00e03e8a
LILKEK361 <115891787+LILKEK361@users.noreply.github.com>
2023-05-08 07:59:18 +0200

    just a bit of design

--- src/lib/components/ui/Navbar.svelte
+++ src/lib/components/ui/Navbar.svelte
@@ -5,14 +5,14 @@
                 <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h7" /></svg>
             </label>
             <ul tabindex="0" class="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52">
-                <li><a>Homepage</a></li>
-                <li><a>Portfolio</a></li>
-                <li><a>About</a></li>
+                <li><a>Page 1</a></li>
+                <li><a>Page 2</a></li>
+                <li><a>Page 3</a></li>
             </ul>
         </div>
     </div>
     <div class="navbar-center">
-        <a class="btn btn-ghost normal-case text-xl">daisyUI</a>
+        <a class="btn btn-ghost normal-case text-xl">Title Here</a>
     </div>
     <div class="navbar-end">
         <button class="btn btn-ghost btn-circle">
