502c9c7ae87d85851accf2af4c17d90d1a9c46f9
LILKEK361 <115891787+LILKEK361@users.noreply.github.com>
2023-05-10 20:06:31 +0200

    Added ProfPictures

--- src/lib/components/ui/Navbar.svelte
+++ src/lib/components/ui/Navbar.svelte
@@ -23,25 +23,25 @@
         <button class="btn btn-ghost btn-circle">
             <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" /></svg>
         </button>
-
         <div class="indicator">
             <div class="dropdown dropdown-end">
                 <label class="btn m-1" tabindex="1">ME</label>
-                <ul class="dropdown-content menu p-2 shadow bg-base-100 rounded-box w-[20vw]" tabindex="1">
+                <ul class="dropdown-content menu p-2 shadow bg-base-100 rounded-box w-[25vw]" tabindex="1">
                     <li>
                         <div class="card card-side bg-base-100 shadow-xl">
 
                             <div class="card-body">
                                 <h2 class="card-title">Yeap thats me: </h2>
-                                <div>
+                                <div class="flex p-3">
                                     <div class="avatar">
                                         <div class="w-24 rounded-full">
-                                            <img src="" />
+                                            <img src="src/lib/components/assets/Nick 02.jpeg" width="auto" height="auto"/>
                                         </div>
+                                        <p class="pl-10">Name: Nick Hillmann <br> Age: 18 <br> Job: None <br> Hobbys: Codding, Video Games</p>
                                     </div>
                                 </div>
                                 <div class="card-actions justify-end">
-                                    <a href="https://github.com/LILKEK361" target="_blank">GitHub</a>
+                                    <a class="bg-secondary rounded-full p-3" href="https://github.com/LILKEK361" target="_blank">GitHub</a>
                                 </div>
                             </div>
                         </div>
