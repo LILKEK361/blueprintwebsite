333da3b336493d97eb24f18733fd7dc8b4952b92
LILKEK361 <115891787+LILKEK361@users.noreply.github.com>
2023-05-11 15:15:59 +0200

    need a better idea for different pages

--- src/lib/components/ui/Navbar.svelte
+++ src/lib/components/ui/Navbar.svelte
@@ -1,20 +1,12 @@
-<script></script>
+<script>
+
+
+
+</script>
 
 <div class="navbar bg-base-100">
     <div class="navbar-start">
-        <div class="dropdown">
-            <label tabindex="0" class="btn btn-ghost btn-circle">
-                <svg class="h-5 w-5" fill="none" stroke="currentColor" viewBox="0 0 24 24"
-                     xmlns="http://www.w3.org/2000/svg">
-                    <path d="M4 6h16M4 12h16M4 18h7" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"/>
-                </svg>
-            </label>
-            <ul tabindex="0" class="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52">
-                <li><a href="../../../routes/+page.svelte">Page 1</a></li>
-                <li><a>Page 2</a></li>
-                <li><a>Page 3</a></li>
-            </ul>
-        </div>
+
     </div>
     <div class="navbar-center">
         <a class="btn btn-ghost normal-case text-xl">Color Buy</a>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,29 +2,24 @@
     import Navbar from "../lib/components/ui/Navbar.svelte";
 
     import ColorTable from "$lib/components/ui/ColorTable.svelte";
-    export let pagenumber = 0;
 
+    let pagenumber = 0;
+    export  function setPage(number){
+        pagenumber = number
+        console.log(pagenumber)
+    }
 
 </script>
 
 
 <div class="overflow-hidden h-[100vh] w-[100vw]">
     <div class="h-[10vh]">
-        <Navbar />
+        <Navbar  />
     </div>
     <div class="flex h-[90vh] w-[100%] items-center justify-center">
         <div class="">
 
-            {#if pagenumber === 0}
-                <ColorTable></ColorTable>
-                {:else if pagenumber === 1}
-                    <ColorTable></ColorTable>
-
-                {:else if pagenumber === 2}
-                    <ColorTable></ColorTable>
-
-
-                {/if}
+            <ColorTable></ColorTable>
 
         </div>
 
