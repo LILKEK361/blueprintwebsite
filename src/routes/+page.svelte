139a3489f05dd86c798057b253a64a8c25d90e09
LILKEK361 <115891787+LILKEK361@users.noreply.github.com>
2023-05-31 16:14:19 +0200

    just ui things

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -11,11 +11,11 @@
 
 <div class="overflow-hidden h-[100vh] w-[100vw]">
     <Navbar pages={["Trending"] }  head="OverAllList"> </Navbar>
-    <div class="flex h-[90vh] w-[100vw] items-center justify-center overflow-x-hidden overflow-y-scroll">
+    <div class="flex h-[90vh] w-[100vw] items-center justify-center overflow-y-hidden ">
 
 
 
-            <OverAllList ></OverAllList>
+        <div class="w-[100%] h-[100%]  overflow-y-scroll"><OverAllList></OverAllList></div>
 
 
 
