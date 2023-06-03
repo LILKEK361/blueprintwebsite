2f843296535dcf332be313667426f479a540cc3a
LILKEK361 <115891787+LILKEK361@users.noreply.github.com>
2023-06-03 14:02:01 +0200

    Update TrendingList.svelte

--- src/lib/components/ui/TrendingList.svelte
+++ src/lib/components/ui/TrendingList.svelte
@@ -5,5 +5,5 @@ import OverAllList from "$lib/components/ui/OverAllList.svelte";
 <div class="flex justify-center items-center w-[100%] h-[100%]" >
 
     <!-- Sorting system -->
-    <div class="w-[75%] h-[90%] border-purple-800 border-2 overflow-y-scroll"><OverAllList></OverAllList></div>
+    <div class=" w-[75%] h-[90%] border-purple-800 border-2 overflow-y-scroll"><OverAllList></OverAllList></div>
 </div>
